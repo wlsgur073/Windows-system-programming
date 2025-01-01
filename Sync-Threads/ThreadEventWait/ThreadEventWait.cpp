d82fb2214562b12092c61efb3752aa426e949476
wlsgur073 <wlsgur073@gmail.com>
2025-01-01 19:44:18 +0900

    Event의 상태는 set, reset이 있고, WaitForSingleObject()는 event의 상태가 변화하면 그것을 감지하고 wait 해제함.
    예시
    1. N1->N2->N3라는 linked list가 있음.
    2. T1와 T2 thread가 해당 연결 리스트에 접근함.
    3. T1이 `N1->N2->N3`를 `N1->N3`로 바꿔버림.
    4. T2는 바껴버린 N2을 접근하지 못해서 `memory access violation`이 발생함.
    
    위와 같은 문제가 발생하는 것을 막기 위해서 Event를 발생시켜 T1에서 중간에 변화가 있었다는 것을 T2에게 알려 사전에 동기화 문제를 방지함.

---
+++ Sync-Threads/ThreadEventWait/ThreadEventWait.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <process.h>
+#include <windows.h>
+
+UINT WINAPI ThreadFunction(LPVOID pParam)
+{
+    HANDLE hEvent = pParam;
+    std::cout << "ThreadFunction() - Begin" << std::endl;
+
+    _getch(); // input char
+    SetEvent(hEvent);
+    std::cout << "ThreadFunction() - SetEvent()" << std::endl;
+    std::cout << "ThreadFunction() - End" << std::endl;
+    return 0;
+}
+
+int main()
+{
+    std::cout << "main() - Begin" << std::endl;
+    HANDLE hEvent = ::CreateEvent(
+          NULL
+        , FALSE // bManualReset: 이벤트로 받는 쪽에서 reset으로 변경되지 않음.
+        , FALSE
+        , NULL); 
+
+    UINT nThreadId = 0;
+    HANDLE hThread = (HANDLE)::_beginthreadex(
+        NULL,
+        0,
+        ThreadFunction,
+        (LPVOID)hEvent,
+        0,
+        &nThreadId);
+    if (hThread == NULL) {
+        std::cout << "ERROR: Failed to begin thread." << std::endl;
+        return 0;
+    }
+    
+    if (::WaitForSingleObject(
+        hEvent, INFINITE) == WAIT_OBJECT_0) // event가 set되기를 기다림.
+    {
+        std::cout << "main() - WaitForSingleObject() return" << std::endl;
+    }
+
+    ::WaitForSingleObject(hThread, INFINITE);
+    std::cout << "main() - Thread End" << std::endl;
+    ::CloseHandle(hThread);
+    std::cout << "main() - End" << std::endl;
+    return 0;
+}
+
+
