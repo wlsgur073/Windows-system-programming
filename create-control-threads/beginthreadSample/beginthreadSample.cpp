a4a65748af183c12888c7166a281489c20e73476
wlsgur073 <wlsgur073@gmail.com>
2025-01-01 13:05:31 +0900

    만약에 T1과 T2가 `g_nData`라는 함수를 접근한다고 생각해보자. T1은 10이라고 쓰고, T2는 20이라고 썼을때 둘 중 하나가 마지막에 실행된 값이 오버라이트 되서 그 값이 `g_nData`의 value로 들어갈 것이다.
    이와같이 **정적 메모리 공간을 썼을 때 동시 접근을 해 원치 않는 값이 나올 문제가 생길 수 있다.** 그래서 스레드별로 메모리 공간 쓰는 걸 분리시키는 `TLS(Thread Local Storage)`를 쓰면 T1과 T2가 쓰는 공간을 별도로 둘 수 있다.
    
    그러나 우리는 많은 라이브러리에 의존하여 코딩을 하게 되는데, 이러한 라이브러리들에서 전역 데이터를 많이 쓰는데 이 변수들과 내가 만든 것들이 충돌이 발생할 수 있다. 이러한 문제를 피하기 위해 기존의 CreateThread와 TLS까지 접목시킨 `_beginthreadex`를 쓰는 것이다.

---
+++ create-control-threads/beginthreadSample/beginthreadSample.cpp
@@ -0,0 +1,34 @@
+﻿// beginthreadSample.cpp : 이 파일에는 'main' 함수가 포함. 거기서 프로그램 실행이 시작되고 종료.
+
+#include <iostream>
+#include <process.h>
+#include <windows.h>
+
+UINT WINAPI ThreadFunction(LPVOID pParam)
+{
+    std::cout << "ThreadFunction() - Begin" << std::endl;
+    std::cout << (const char*)pParam << std::endl;
+    std::cout << "ThreadFunction() - End" << std::endl;
+    return 0;
+}
+
+int main()
+{
+    std::cout << "main() - Begin" << std::endl;
+    UINT nThreadId = 0;
+    HANDLE hThread = (HANDLE)::_beginthreadex(
+        NULL,
+        0,
+        ThreadFunction,
+        (LPVOID)"PARAM",
+        0,
+        &nThreadId);
+    if (hThread == NULL) {
+    }
+
+    ::WaitForSingleObject(hThread, INFINITE);
+    ::CloseHandle(hThread);
+
+    std::cout << "main() - End" << std::endl;
+    return 0;
+}
