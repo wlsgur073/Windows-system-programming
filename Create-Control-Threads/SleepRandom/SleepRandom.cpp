8fa55582541af04e32776eb3516ae1c0fb9059c2
wlsgur073 <wlsgur073@gmail.com>
2025-01-01 13:58:36 +0900

    OS가 Thread를 스케줄링하고 Thread가 CPU를 쓰는 동안 Context Switching이 발생하며, 자연스럽게 Overhead가 발생하고 그에 따른 대기시간이 발생하는데, 이때 sleep(1)을 했다고 1ms만 대기하는지 그 이상 대기하는지 알 수 없다.
    정리하여 멀티스레드의 역학구조를 따지고 보면 Sleep()으로 인해 프로세스가 꼬일 가능성이 매우 크다.
    
    suspend되고 resume되는 시간이 ms인데 보통 그것에 알파이상인데, 이것은 알 수 없다고 했는데 유추할 수 있는 방법이 있다.

---
+++ Create-Control-Threads/SleepRandom/SleepRandom.cpp
@@ -0,0 +1,40 @@
+﻿// SleepRandom.cpp : 이 파일에는 'main' 함수가 포함. 거기서 프로그램 실행이 시작되고 종료.
+
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+	LARGE_INTEGER freq;
+	LARGE_INTEGER begin;
+	LARGE_INTEGER end;
+	__int64 elapsed;
+
+	//CPU 타이머 주파수 확인
+	::QueryPerformanceFrequency(&freq);
+	std::cout << "초당 주파수: " << freq.QuadPart << std::endl;
+
+
+	for (int i = 0; i < 5; ++i)
+	{
+		::QueryPerformanceCounter(&begin);
+		////////////////////////////////////////////////////////
+		//우연에 맡기기 위한 코드 한 줄
+		::Sleep(10); // 10ms
+		////////////////////////////////////////////////////////
+		::QueryPerformanceCounter(&end);
+
+		elapsed = end.QuadPart - begin.QuadPart;
+
+		std::cout << "실제로 흘러간 시간:" <<
+			elapsed << std::endl;
+		std::cout << "실제로 흘러간 시간(ms):" <<
+			(double)elapsed / freq.QuadPart * 1000 << std::endl;
+		std::cout << "실제로 흘러간 시간(micro):" <<
+			(double)elapsed / freq.QuadPart * 1000 * 1000 << std::endl;
+		std::cout << "랜덤 값(0~100):" <<
+			elapsed % 100 << std::endl;
+	}
+
+	return 0;
+}
