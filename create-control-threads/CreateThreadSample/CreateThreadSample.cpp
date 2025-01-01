d6108e4ad8c40c9ff977f629f3f4d352e2b31f73
wlsgur073 <wlsgur073@gmail.com>
2025-01-01 12:47:04 +0900

    ### CreateThread() - lpThreadAttributes: 첫 번째 매개변수인 lpThreadAttributes에는 특별한 이유가 없다면 null을 집어 넣는다. 권한, 보안 속성은 지속적인 상속을 통해서 계승되기 때문에 특별한 이유가 없으면 null로 처리하는 것이다.
    - dwStackSize: Thread는 기본적으로 스택 구조로 메모리를 가지고 컴파일러 기본 옵션이 1MB로 돼 있다. 따라서 두 번째 매개변수인 dwStackSize에 0을 넣어주면 기본값으로 설정된다.
    
    - lpStartAddress: 타입이 LP로 시작되는데 이것은 포인터를 받는 것을 알 수 있다. 따라서 Thread로 실행할 수 있는 함수의 주소를 넣어 달라는 의미이다.
    
    - dwCreationFlags: 설정하지 않으면 Thread가 호출되자마자 실행된다.
    
    - lpThreadId: 해당 스레드의 id값을 return한다.
    
    `CREATE_SUSPENDED` 상태로 만들어서 생성하면 스레드가 생성하되, 바로 실행되지 않고 `ResumeThread` 함수를 이용해서 실행시킬 수 있다.
    
    #### WaitForSingleObject(hHandle, dwMilliseconds):
    - hHandle: 핸들이라는게 포인터인데, 그것을 가리키는 대상 메모리가 있을텐데, 거기에 어떠한 변화가 생기면 return을 하게 된다.
    
    - dwMilliseconds: hHandle를 얼마동안 기다릴건지 결정하는 것이 두 번째 매개변수인 INFINITE이다. 참고로 INFINITE로 설정하면 hHandle가 끝날때까지 무한정으로 기다리다가 반환한다.
    
    `WaitForSingleObject()`는 동기화할때 쓰는 가장 대표적인 함수이다. object가 handle이고 handle은 pointer이고 pointer은 어떤 메모리 주소 값을 가지고 있고, 그 메모리 주소에 변화가 생긴다면 함수가 반환한다.
    
    * https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-resumethread
    * https://learn.microsoft.com/en-us/windows/win32/api/errhandlingapi/nf-errhandlingapi-getlasterror

---
+++ create-control-threads/CreateThreadSample/CreateThreadSample.cpp
@@ -0,0 +1,37 @@
+﻿// CreateThreadSample.cpp : 이 파일에는 'main' 함수가 포함. 거기서 프로그램 실행이 시작되고 종료.
+
+#include <iostream>
+#include <windows.h>
+
+// WINAPI는 __stdcall이다.
+// __stdcall의 특징: 스택 정리 책임, 파라미터 전달 순서(right to left), 함수 이름 맹글링
+DWORD WINAPI ThreadFunction(LPVOID pParam)
+{
+    std::cout << "ThreadFunction() - Begin" << std::endl;
+    std::cout << (const char*)pParam << std::endl;
+    std::cout << "ThreadFunction() - End" << std::endl;
+    return 0;
+}
+
+int main()
+{
+    std::cout << "main() - Begin" << std::endl;
+    DWORD dwThreadId = 0;
+    HANDLE hThread = ::CreateThread(
+        NULL,
+        0,
+        ThreadFunction, // 생성이 되면서 자식 스레드가 됨.
+        (LPVOID)"PARAM",
+        0,
+        &dwThreadId);
+    if (hThread == NULL) {
+    }
+
+    std::cout << "main() - WaitForSingleObject() - before" << std::endl;
+
+    ::WaitForSingleObject(hThread, INFINITE);
+    ::CloseHandle(hThread);
+
+    std::cout << "main() - End" << std::endl;
+    return 0;
+}
