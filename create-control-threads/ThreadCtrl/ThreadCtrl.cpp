ff8c0368c72d8c830fd784420ec617e5f566c05f
wlsgur073 <wlsgur073@gmail.com>
2025-01-01 13:32:11 +0900

    눈에 보이는 Suspend와 Resume되는 시점으로 코드가 중단되고 다시 재기동된다고 봐서는 안된다. 디어셈블리해보면 while문 하나 실행하는데에도 여러 동작이 오고 가기에, 정확한 측정이 필요하다면 디버깅으로 면밀하게 검사할 필요가 있다.
    어쨌든, 여기서 중요한건 Suspend와 Resume를 통해 Thread를 컨트롤 할 수 있다는 것만 알아두자.

---
+++ create-control-threads/ThreadCtrl/ThreadCtrl.cpp
@@ -0,0 +1,61 @@
+﻿// ThreadCtrl.cpp : 이 파일에는 'main' 함수가 포함. 거기서 프로그램 실행이 시작되고 종료.
+
+#include <iostream>
+#include <process.h>
+#include <windows.h>
+
+int g_bFlag = FALSE;
+
+UINT WINAPI ThreadFunction(LPVOID pParam)
+{
+    std::cout << "ThreadFunction - begin" << std::endl;
+    g_bFlag = TRUE;
+    while (g_bFlag)
+    {
+        std::cout << "Worker thread - while loop" << std::endl;
+        ::Sleep(1000);
+    }
+
+    std::cout << "ThreadFunction - End" << std::endl;
+    return 0;
+}
+
+int main()
+{
+    std::cout << "main() - Begin" << std::endl;
+    UINT nThreadId = 0;
+    HANDLE hThread = (HANDLE)::_beginthreadex(
+        NULL,
+        0,
+        ThreadFunction,
+        (LPVOID)"PARAM",
+        0,
+        &nThreadId);
+    if (hThread == NULL) {
+        std::cout << "ERROR: Failed to begin thread." << std::endl;
+        return 0;
+    }
+
+    ::Sleep(2000);
+
+    std::cout << "main() - SuspendThread()" << std::endl;
+    ::SuspendThread(hThread); // no execute
+
+    for (int i = 0; i < 3; ++i)
+    {
+        ::WaitForSingleObject(hThread, 1000);
+        std::cout << "main() - WaitForSingleObject(1000)" << std::endl;
+    }
+
+    ::ResumeThread(hThread); // execute
+    std::cout << "main() - ResumeThread()" << std::endl;
+
+    ::Sleep(2000);
+    g_bFlag = FALSE;
+    ::WaitForSingleObject(hThread, INFINITE);
+    ::CloseHandle(hThread);
+
+    std::cout << "main() - End" << std::endl;
+    return 0;
+}
+
