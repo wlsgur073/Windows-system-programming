91aa08c2d57e0c7fbe384ca840597825a98bfa53
wlsgur073 <wlsgur073@gmail.com>
2025-01-02 21:49:53 +0900

    const String? read-only char array.

---
+++ Memory-Operations/SysInfoSample/SysInfoSample.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <tchar.h>
+#include <windows.h>
+
+int main()
+{
+	SYSTEM_INFO si = { 0 };
+
+	::GetSystemInfo(&si);
+	_tprintf(_T("Page size: %d\n"), si.dwPageSize);
+	_tprintf(_T("Allocation size: %d\n"), si.dwAllocationGranularity);
+}
---
+++ Memory-Operations/VirtualMemSample/VirtualMemSample.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <tchar.h>
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	const char* pszHello = "Hello, World!"; // read-only memory
+    printf("%s\n", pszHello);
+
+    DWORD dwOldProtect = 0;
+    ::VirtualProtect((LPVOID)pszHello,
+		16, PAGE_READWRITE, &dwOldProtect); // change the memory protection
+
+	strcpy_s((char*)pszHello, 16, "nullnull!=null"); // write to read-only memory
+    printf("%s\n", pszHello);
+}
+
