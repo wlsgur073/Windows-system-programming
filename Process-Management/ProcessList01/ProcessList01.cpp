f0bfb46037642fef40d04e80f56d0f625c282e78
wlsgur073 <wlsgur073@gmail.com>
2025-01-03 17:31:07 +0900

    Use snapshots to enumerate running processes for administrative purposes.
    p.s. 코딩테스트 한번 보고 나면 기가 빨린다..

---
+++ Process-Management/ProcessList01/ProcessList01.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <tchar.h>
+#include <windows.h>
+#include <psapi.h>
+
+int main()
+{
+	_wsetlocale(LC_ALL, L"korean");
+
+	DWORD		pidArray[512] = { 0 };
+	DWORD		cbNeeded = 0;
+	DWORD		nProcesses = 0;
+	TCHAR		szBuffer[MAX_PATH + _MAX_FNAME] = { 0 };
+	UINT		uCounter = 0;
+
+
+	if (::EnumProcesses(pidArray, sizeof(pidArray), &cbNeeded))
+	{
+		nProcesses = cbNeeded / sizeof(DWORD);
+		for (DWORD i = 0; i <= nProcesses; i++)
+		{
+			HANDLE hProcess;
+			DWORD pid = pidArray[i];
+
+			hProcess = ::OpenProcess( // system permission is stronger than the administrator
+				PROCESS_QUERY_INFORMATION | PROCESS_VM_READ // Request those permissions
+				, FALSE
+				, pid
+			);
+			if (hProcess != NULL)
+			{
+				::ZeroMemory(szBuffer, MAX_PATH + _MAX_FNAME);
+				if (::GetModuleFileNameEx(hProcess, NULL,
+					szBuffer, MAX_PATH + _MAX_FNAME) != NULL)
+				{
+					_tprintf(TEXT("[PID: %d] %s\n"), pid, szBuffer);
+				}
+				::CloseHandle(hProcess);
+			}
+			else
+			{
+				_tprintf(TEXT("[PID: %d, Error code: %d]\n"),
+					pid, ::GetLastError()); // error code 5: Access Denied
+				// you can search the error code in `Tools > Error Lookup`
+				continue;
+			}
+		}
+	}
+
+	return 0;
+}
---
+++ Process-Management/ProcessList02/ProcessList02.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+#include <tchar.h>
+#include <windows.h>
+#include <tlhelp32.h>
+
+int main()
+{
+	_wsetlocale(LC_ALL, L"korean");
+	HANDLE		hSnapshot = INVALID_HANDLE_VALUE;
+
+	hSnapshot = ::CreateToolhelp32Snapshot( // Create a snapshot of the system
+		TH32CS_SNAPPROCESS // Take a snapshot of the system
+		, NULL // Take a snapshot of the entire system
+	);
+	if (hSnapshot != INVALID_HANDLE_VALUE)
+	{
+		PROCESSENTRY32 pe32 = { sizeof(pe32) };
+
+		BOOL bFlag = ::Process32First(hSnapshot, &pe32);
+		for (; bFlag; bFlag = ::Process32Next(hSnapshot, &pe32))
+			_tprintf(TEXT("[PID: %d] %s\n"),
+				pe32.th32ProcessID, pe32.szExeFile);
+		::CloseHandle(hSnapshot);
+	}
+	
+	return 0;
+}
