89240b0c1f15e7943534936fdf8c3843088a4752
wlsgur073 <wlsgur073@gmail.com>
2025-01-03 19:20:34 +0900

    IPC(Inter-process communication): 영문 그대로 해석하면 됨. 내부 프로세스의 커뮤니케이션임. 여기서 내부란 kernel을 뜻함.
    IPC 기법도 다양하지만, 기본적으로 해당 코드로는 파일 매핑 객체(공유 메모리)를 이용하여 프로세스 간의 동기화를 하는 것으로 만듦.
    
    IPC로 하면 socket보다는 빠름. 이유는 socket은 결국 던지고 받는 행를 거쳐야 하지만, IPC는 포인터를 통해 결국 메모리 주소를 직접 접근하는 방식으로 속도는 빠른데, 그만큼 깊은 지식을 요구함.

---
+++ Process-Management/SharedMemClient/SharedMemClient.cpp
@@ -0,0 +1,125 @@
+﻿#include <iostream>
+#include <tchar.h>
+#include <windows.h>
+
+int main()
+{
+	// debugview를 이용하여 출력된 문자열을 확인할 수 있음
+	OutputDebugString(_T("Client - begin\n"));
+
+	//1. 파일 매핑 객체 생성(혹은 개방)
+	HANDLE hMap = ::CreateFileMapping(
+		INVALID_HANDLE_VALUE, 
+		NULL,
+		PAGE_READWRITE, // read/write
+		0,
+		128, // shared memory size in kernel
+		TEXT("IPC_TEST_SHARED_MEMORY"));
+	if (GetLastError() == ERROR_ALREADY_EXISTS)
+	{
+		//파일 매핑 객체가 이미 생성되어 있으므로 기존 것을 오픈
+		hMap = ::OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE,
+			TEXT("IPC_TEST_SHARED_MEMORY"));
+		if (hMap == NULL)
+		{
+			_tprintf(TEXT("Failed to open file mapping obj. (%d)\n"),
+				::GetLastError());
+			return FALSE;
+		}
+	}
+	 
+	//2. 매핑 객체에 대한 접근 포인터를 얻음. (메모리 추상화)
+	TCHAR *pSharedMemory = (TCHAR*)::MapViewOfFile(
+		hMap, FILE_MAP_ALL_ACCESS, 0, 0, 128);
+	if (pSharedMemory == NULL)
+	{
+		_tprintf(TEXT("Failed to get shared memory. (%d)\n"), // unicode string
+			::GetLastError());
+		::CloseHandle(hMap);
+		return 0;
+	}
+
+	//3. 공유 메모리 접근 및 읽기(수신 완료) 이벤트 생성(혹은 개방)
+	HANDLE hEvent = ::CreateEvent(
+		NULL, TRUE, FALSE, TEXT("IPC_SHAREDMEM_ACCESS"));
+	if (::GetLastError() == ERROR_ALREADY_EXISTS)
+	{
+		hEvent = ::OpenEvent(
+			EVENT_ALL_ACCESS, FALSE, TEXT("IPC_SHAREDMEM_ACCESS"));
+		if (hEvent == NULL)
+		{
+			_tprintf(TEXT("Failed to open event obj. (%d)\n"),
+				::GetLastError());
+
+			::UnmapViewOfFile(pSharedMemory);
+			::CloseHandle(hMap);
+			return 0;
+		}
+	}
+
+	HANDLE hEvtComplete = ::CreateEvent(
+		NULL, TRUE, FALSE, TEXT("IPC_SHAREDMEM_RECV_COMPLETE")); // completion event
+	if (::GetLastError() == ERROR_ALREADY_EXISTS)
+	{
+		hEvtComplete = ::OpenEvent(
+			EVENT_ALL_ACCESS, FALSE, TEXT("IPC_SHAREDMEM_RECV_COMPLETE"));
+		if (hEvent == NULL)
+		{
+			_tprintf(TEXT("Failed to open event obj. (%d)\n"),
+				::GetLastError());
+
+			::UnmapViewOfFile(pSharedMemory);
+			::CloseHandle(hMap);
+			::CloseHandle(hEvent);
+			return 0;
+		}
+	}
+
+	//4. 프로세스 동기화를 위한 뮤텍스 생성
+	HANDLE hMutex = ::CreateMutex(NULL, FALSE,
+		TEXT("IPC_SHAREDMEM_MUTEX"));
+	if (::GetLastError() == ERROR_ALREADY_EXISTS)
+	{
+		hMutex = ::OpenMutex(MUTEX_ALL_ACCESS, FALSE,
+			TEXT("IPC_SHAREDMEM_MUTEX"));
+		if(hMutex == NULL)
+		{
+			_tprintf(TEXT("Failed to open mutex obj. (%d)\n"),
+				::GetLastError());
+
+			::UnmapViewOfFile(pSharedMemory);
+			::CloseHandle(hMap);
+			::CloseHandle(hEvent);
+			::CloseHandle(hEvtComplete);
+			return 0;
+		}
+	}
+
+	//5. 공유 메모리 쓰기 이벤트 대기
+	OutputDebugString(TEXT("Waiting message from server...\n"));
+	if (::WaitForSingleObject(hEvent, INFINITE) == WAIT_OBJECT_0) // wait for the event from the server
+	{
+		//6. 뮤텍스를 이용한 프로세스 동기화 (메모리 접근)
+		if(::WaitForSingleObject(hMutex, INFINITE) != WAIT_OBJECT_0)
+			OutputDebugString(TEXT("MUTEX error.\n"));
+		else
+			OutputDebugString(TEXT("MUTEX Lock.\n")); // like a critical section start
+
+		OutputDebugString(pSharedMemory);
+		::ReleaseMutex(hMutex);
+		OutputDebugString(TEXT("MUTEX Unlock.\n")); // // like a critical section end
+
+		//7. 메모리에 읽기 완료 이벤트 셋
+		::SetEvent(hEvtComplete);
+		OutputDebugString(_T("Completion event for server!\n"));
+	}
+
+	::UnmapViewOfFile(pSharedMemory);
+	::CloseHandle(hMap);
+	::CloseHandle(hEvent);
+	::CloseHandle(hEvtComplete);
+	::CloseHandle(hMutex);
+
+	_tprintf(_T("Client - end\n"));
+	return 0;
+}
---
+++ Process-Management/SharedMemServer/SharedMemServer.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+#include <tchar.h>
+#include <windows.h>
+
+int main()
+{
+	OutputDebugString(_T("Server - begin\n"));
+
+	//1. 파일 매핑 객체 생성(혹은 개방)
+	HANDLE hMap = ::CreateFileMapping(INVALID_HANDLE_VALUE, NULL,
+		PAGE_READWRITE,
+		0,
+		128,
+		TEXT("IPC_TEST_SHARED_MEMORY"));
+	if (GetLastError() == ERROR_ALREADY_EXISTS)
+	{
+		//파일 매핑 객체가 이미 생성되어 있으므로 기존 것을 오픈
+		hMap = ::OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE,
+			TEXT("IPC_TEST_SHARED_MEMORY"));
+		if (hMap == NULL)
+		{
+			_tprintf(TEXT("Failed to open file mapping obj. (%d)\n"),
+				::GetLastError());
+			return FALSE;
+		}
+	}
+
+	//2. 매핑 객체에 대한 접근 포인터를 얻음. (메모리 추상화)
+	TCHAR* pSharedMemory = (TCHAR*)::MapViewOfFile(
+		hMap, FILE_MAP_ALL_ACCESS, 0, 0, 128);
+	if (pSharedMemory == NULL)
+	{
+		_tprintf(TEXT("Failed to get shared memory. (%d)\n"),
+			::GetLastError());
+		::CloseHandle(hMap);
+		return 0;
+	}
+
+	//3. 공유 메모리 접근 및 읽기(수신 완료) 이벤트 생성(혹은 개방)
+	HANDLE hEvent = ::CreateEvent(
+		NULL, TRUE, FALSE, TEXT("IPC_SHAREDMEM_ACCESS"));
+	if (::GetLastError() == ERROR_ALREADY_EXISTS)
+	{
+		hEvent = ::OpenEvent(
+			EVENT_ALL_ACCESS, FALSE, TEXT("IPC_SHAREDMEM_ACCESS"));
+		if (hEvent == NULL)
+		{
+			_tprintf(TEXT("Failed to open event obj. (%d)\n"),
+				::GetLastError());
+
+			::UnmapViewOfFile(pSharedMemory);
+			::CloseHandle(hMap);
+			return 0;
+		}
+	}
+
+	HANDLE hEvtComplete = ::CreateEvent(
+		NULL, TRUE, FALSE, TEXT("IPC_SHAREDMEM_RECV_COMPLETE"));
+	if (::GetLastError() == ERROR_ALREADY_EXISTS)
+	{
+		hEvtComplete = ::OpenEvent(
+			EVENT_ALL_ACCESS, FALSE, TEXT("IPC_SHAREDMEM_RECV_COMPLETE"));
+		if (hEvtComplete == NULL)
+		{
+			_tprintf(TEXT("Failed to open event obj. (%d)\n"),
+				::GetLastError());
+
+			::UnmapViewOfFile(pSharedMemory);
+			::CloseHandle(hMap);
+			::CloseHandle(hEvent);
+			return 0;
+		}
+	}
+
+	//4. 프로세스 동기화를 위한 뮤텍스 생성
+	HANDLE hMutex = ::CreateMutex(NULL, FALSE,
+		TEXT("IPC_SHAREDMEM_MUTEX"));
+	if (::GetLastError() == ERROR_ALREADY_EXISTS)
+	{
+		hMutex = ::OpenMutex(MUTEX_ALL_ACCESS, FALSE,
+			TEXT("IPC_SHAREDMEM_MUTEX"));
+		if (hMutex == NULL)
+		{
+			_tprintf(TEXT("Failed to open mutex obj. (%d)\n"),
+				::GetLastError());
+
+			::UnmapViewOfFile(pSharedMemory);
+			::CloseHandle(hMap);
+			::CloseHandle(hEvent);
+			::CloseHandle(hEvtComplete);
+			return 0;
+		}
+	}
+
+	//5. 뮤텍스를 이용한 프로세스 동기화 (메모리 접근) // only this part is different from the client
+	if(::WaitForSingleObject(hMutex, INFINITE) != WAIT_OBJECT_0)
+		OutputDebugString(TEXT("MUTEX error.\n"));
+	else
+		OutputDebugString(TEXT("MUTEX Lock.\n"));
+
+	wsprintfW(pSharedMemory, _T("%s"), _T("Hello, Shared memory!"));
+	::ReleaseMutex(hMutex);
+	OutputDebugString(TEXT("MUTEX Unlock.\n"));
+
+	//6. 공유 메모리 쓰기 접근 및 쓰기 이벤트 세트
+	OutputDebugString(TEXT("Message event set.\n"));
+	::SetEvent(hEvent);
+
+	//7. 클라이언트 수신 완료 이벤트 대기
+	OutputDebugString(TEXT("Waiting recv event...\n"));
+	if (::WaitForSingleObject(hEvtComplete, INFINITE) == WAIT_OBJECT_0)
+		OutputDebugString(_T("Completion event!\n"));
+
+	::UnmapViewOfFile(pSharedMemory);
+	::CloseHandle(hMap);
+	::CloseHandle(hEvent);
+	::CloseHandle(hEvtComplete);
+	::CloseHandle(hMutex);
+
+	OutputDebugString(_T("Server - end\n"));
+	return 0;
+}
